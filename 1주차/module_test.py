80fc49c8a69b834e10c1952decd9f141f195bdd8
박채희 <chaeheepark99@gmail.com>
2021-06-29 21:26:56 +0900

    module_test.py

---
+++ 1주차/module_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def mean(nums):
+    return sum(nums)/len(nums)
+
+def calc(x,y):
+    add=x+y
+    sub=x-y
+    mult=x*y
+    div=x/y
+    return add, sub, mult,div
+
